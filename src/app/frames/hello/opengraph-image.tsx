3a8b14ae0d5d715df49fb6bebc79a42ea6ab3a38
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-10 05:14:06 -0600

    hfjhjv

--- src/app/frames/hello/opengraph-image.tsx
+++ src/app/frames/hello/opengraph-image.tsx
@@ -13,8 +13,8 @@ export const contentType = "image/png";
 export default async function Image() {
   return new ImageResponse(
     (
-      <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-6xl">Hello, world!</h1>
+      <div tw="h-full w-full flex flex-col justify-center items-center relative" style={{ backgroundColor: '#9d00ff' }}>
+        <h1 tw="text-6xl text-white">Tic-Tac-Maxi</h1>
       </div>
     ),
     {
