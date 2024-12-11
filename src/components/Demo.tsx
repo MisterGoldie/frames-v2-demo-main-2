adcf711bf7deb0aef3eac447f8264a2ea82eafdb
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-11 12:16:38 -0600

    GHXHGCH

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -448,7 +448,7 @@ export default function Demo({ tokenBalance }: DemoProps) {
                 Tic-Tac-Maxi
               </Button>
               {tokenBalance > 0 && (
-                <div className="mt-10 bg-purple-600 text-white px-3 py-1 rounded-full text-sm inline-flex items-center">
+                <div className="mt-12 bg-purple-600 text-white px-3 py-1 rounded-full text-sm inline-flex items-center">
                   <Image 
                     src="/fantokenlogo.png"
                     alt="Fan Token"
