d46154f360d227815b387e4ca6880d634aa0ba34
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-12 23:49:20 -0600

    sfvsbsbs

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,18 +1,20 @@
-"use client";
-
-export default function Snow() {
+const Snow = () => {
   return (
     <div className="snow-container">
-      {[...Array(50)].map((_, i) => (
-        <div
-          key={i}
-          className={`snow ${i % 3 === 1 ? 'snow-1' : i % 3 === 2 ? 'snow-2' : ''}`}
+      {[...Array(200)].map((_, index) => (
+        <div 
+          key={index} 
+          className={`snow snow-${index % 3}`}
           style={{
-            left: `${Math.random() * 100}vw`,
-            animationDelay: `${Math.random() * -15}s`,
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 3 + 2}s`,
+            animationDelay: `${Math.random() * 2}s`,
+            fontSize: `${Math.random() * 10 + 5}px`
           }}
         />
       ))}
     </div>
   );
-} 
+};
+
+export default Snow; 
