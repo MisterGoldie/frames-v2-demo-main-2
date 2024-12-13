c41521e0988dc45fb9e5846e0b367764a29f4a1e
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-12 22:58:23 -0600

    JHVJHVJ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ const appUrl = 'https://podplayv2.vercel.app';
 
 const frame = {
   version: "next",
-  imageUrl: `${appUrl}/urlimage.png`,
+  imageUrl: `${appUrl}/game-board.png`,
   button: {
     title: "Play Now",
     action: {
       type: "launch_frame",
       name: "POD Play v2",
       url: appUrl,
-      splashImageUrl: `${appUrl}/urlimage.png`,
+      splashImageUrl: `${appUrl}/game-board.png`,
       splashBackgroundColor: "#345fa8",
     },
   },
